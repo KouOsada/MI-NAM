06977a523a925fc04b666b8e29d08ed8b0e62bf5
KouOsada <kouosada.0930@gmail.com>
2021-11-04 06:19:31 +0000

    deviseの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  # デバイス管理者側
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  # デバイスユーザー側
+  devise_for :user, skip: [:passwords], controllers: {
+    registrations: "user/registrations",
+    sessions: "user/sessions"
+  }
+  
+  # 管理者側ルーティング
+  
+  # ユーザー側ルーティング
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
