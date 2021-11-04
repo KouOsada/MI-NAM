ba0197a7a829e9a58a738631ce21085b616b69b9
KouOsada <kouosada.0930@gmail.com>
2021-11-04 07:26:43 +0000

    rails/deviseの日本語化

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,20 @@ Rails.application.routes.draw do
   }
   
   # 管理者側ルーティング
+  namespace :admin do
+    resources :users, only: [:index, :show]
+    resources :posts, only: [:index, :show]
+    resources :genres, only: [:create, :index, :edit, :update, :destroy]
+  end
   
   # ユーザー側ルーティング
+  scope module: :user do
+    root to: "homes#top"
+    resources :posts do
+    resources :comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+  end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
