396b821ed26f1f4abe13853051e57121d8b834e3
KouOsada <kouosada.0930@gmail.com>
2021-11-04 10:23:47 +0000

    ユーザーの新規登録機能途中

--- config/routes.rb
+++ config/routes.rb
@@ -21,9 +21,9 @@ Rails.application.routes.draw do
   scope module: :user do
     root to: "homes#top"
     resources :posts do
-    resources :comments, only: [:create, :destroy]
-    resource :favorites, only: [:create, :destroy]
-  end
+      resources :comments, only: [:create, :destroy]
+      resource :favorites, only: [:create, :destroy]
+    end
   end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
