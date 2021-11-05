6f5d960a987142ddc923d407d318bb457607f020
KouOsada <kouosada.0930@gmail.com>
2021-11-05 05:41:13 +0000

    ユーザーの新規登録画面終了

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
       resources :comments, only: [:create, :destroy]
       resource :favorites, only: [:create, :destroy]
     end
+    resources :users, only: [:show, :edit, :update]
   end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
