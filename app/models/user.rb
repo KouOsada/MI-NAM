0c3439481313aa27dcfd2da800a3cd36eca1e9ae
KouOsada <kouosada.0930@gmail.com>
2021-11-06 10:29:54 +0000

    [Add]投稿機能/user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
   attachment :profile_image
   
-  # 年齢　0:10代 1:20代 2:30代 3:40代 4:50代以上 
+  # 投稿関連のアソシエーション
+  has_many :posts, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  
+  # enumの設定
+  # 　年齢　0:10代 1:20代 2:30代 3:40代 4:50代以上 
   enum age: { teen: 0, twenties: 1, thirties: 2, forties: 3, over_fifties: 4 }
-  # 肌タイプ　0:普通肌 1:乾燥肌 2:脂性肌 3:混合肌 4:敏感肌
+  # 　肌タイプ　0:普通肌 1:乾燥肌 2:脂性肌 3:混合肌 4:敏感肌
   enum skin_type: { normal: 0, dry: 1, oily: 2, combination: 3, sensitive: 4 }
+  
+  # バリデーション
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_04_052739) do
+ActiveRecord::Schema.define(version: 2021_11_06_042504) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,22 @@ ActiveRecord::Schema.define(version: 2021_11_04_052739) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "genre_id", null: false
+    t.string "title", null: false
+    t.text "body", null: false
+    t.string "post_image_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
