2d07cff008df691dc01fcf7c5fbd774ba2fac8d0
KouOsada <kouosada.0930@gmail.com>
2021-11-07 03:49:17 +0000

    [Fix]ユーザー・管理者のエラー

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,7 +1,11 @@
 class Admin::PostsController < ApplicationController
+  before_action :authenticate_admin!
+  
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 end
