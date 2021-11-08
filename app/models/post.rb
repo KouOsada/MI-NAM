ebbda0933c8e05205425ca24c6c2fd370beabcb9
KouOsada <kouosada.0930@gmail.com>
2021-11-08 06:02:57 +0000

    [Add]非同期いいね機能/user

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,12 @@ class Post < ApplicationRecord
   # 投稿関連のアソシエーション
   belongs_to :user
   belongs_to :genre
+  has_many :favorites, dependent: :destroy
+  
+  #お気に入りにしているかの確認メソッド
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 
   attachment :post_image
 
--- app/models/post_hashtag.rb
+++ app/models/post_hashtag.rb
@@ -1,4 +1,9 @@
 class PostHashtag < ApplicationRecord
+  
+  #お気に入りにしているかの確認メソッド
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 
   # バリデーション
   validates :post_id, presence: true
