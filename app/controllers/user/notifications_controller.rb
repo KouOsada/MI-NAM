8280f7088997739d2b8577af25edc4e8b986fbb2
KouOsada <kouosada.0930@gmail.com>
2021-11-11 07:04:04 +0000

    [Add]通知一覧画面

--- app/controllers/user/notifications_controller.rb
+++ app/controllers/user/notifications_controller.rb
@@ -1,11 +1,11 @@
 class User::NotificationsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @notifications = current_user.passive_notifications.all
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
     end
-    
-  
+  end
+
 end
