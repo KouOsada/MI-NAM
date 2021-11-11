2616074d2cff417f16eadb42c4a465a980bccfb2
KouOsada <kouosada.0930@gmail.com>
2021-11-11 05:46:12 +0000

    [Add]通知のメソッド作成

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,7 @@ class Comment < ApplicationRecord
   #アソシエーション
   belongs_to :user
   belongs_to :post
+  has_many :notifications, dependent: :destroy
   
   validates :comment, presence: true, length: {maximum: 150}
   
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,2 +1,11 @@
 class Notification < ApplicationRecord
+  
+  # デフォルトの並び順を作成日時の降順で指定
+  default_scope -> { order(created_at: :desc) }
+  # optional: true でnilを許可する
+  belongs_to :post, optional: true
+  belongs_to :comment, optional: true
+  belongs_to :visitor, class_name: "User", foreign_key: "visitor_id", optional: true
+  belongs_to :visited, class_name: "User", foreign_key: "visited_id", optional: true
+  
 end
