fa7529a76181c721f20af4056cd32b548273dd18
KouOsada <kouosada.0930@gmail.com>
2021-11-16 04:47:00 +0000

    [Add]評価用のカラム

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_11_044609) do
+ActiveRecord::Schema.define(version: 2021_11_16_044526) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 2021_11_11_044609) do
     t.string "post_image_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "evaluation"
   end
 
   create_table "relationships", force: :cascade do |t|
