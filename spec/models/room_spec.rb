d9c5e26dd2dc5a268ee5e405542020118d1e7cef
KouOsada <kouosada.0930@gmail.com>
2022-01-16 11:47:32 +0000

    [Add]Roomモデルのテスト

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -3,5 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe 'モデルのテスト', type: :model do
-  
+  describe 'アソシエーションのテスト' do
+    context 'Entryモデルとの関係' do
+      it '1:Nとなっているか' do
+        expect(Relationship.reflect_on_association(:entry).macro).to eq :has_many
+      end
+    end
+    context 'Messageモデルとの関係' do
+      it '1:Nとなっているか' do
+        expect(Relationship.reflect_on_association(:message).macro).to eq :has_many
+      end
+    end
+    context 'Notificationモデルとの関係' do
+      it '1:Nとなっているか' do
+        expect(Relationship.reflect_on_association(:notification).macro).to eq :has_many
+      end
+    end
+  end
 end
