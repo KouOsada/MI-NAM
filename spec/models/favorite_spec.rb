c02d6aabd38a49d58a1b226f75a7e93fdd50440f
KouOsada <kouosada.0930@gmail.com>
2022-01-16 10:48:07 +0000

    [Add]Favoriteモデルのテスト

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -2,6 +2,17 @@
 
 require 'rails_helper'
 
-RSpec.describe '', type: :model do
-  
+RSpec.describe 'Favoriteモデルのテスト', type: :model do
+  describe 'アソシエーションのテスト' do
+    context 'Userモデルとの関係' do
+      it '1:Nとなっているか' do
+        expect(Favorite.reflect_on_association(:user).macro).to eq :belongs_to
+      end
+    end
+    context 'Postモデルとの関係' do
+      it '1:Nとなっているか' do
+        expect(Favorite.reflect_on_association(:).macro).to eq :belongs_to
+      end
+    end
+  end
 end
