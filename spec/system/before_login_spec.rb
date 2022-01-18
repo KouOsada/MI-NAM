2790f27dcfcaf43bdad01f90650a6ed065bb0a53
KouOsada <kouosada.0930@gmail.com>
2022-01-18 10:52:07 +0000

    [Update]微修正

--- spec/system/before_login_spec.rb
+++ spec/system/before_login_spec.rb
@@ -12,37 +12,37 @@ describe 'ユーザーログイン前のテスト' do
       it 'URLが正しいか' do
           expect(current_path).to eq '/'
       end
-      it 'アバウト画面のリンクが表示されている' do
-          about_link = find_all('a')[2].native.inner_text
-          expect(about_link).to match(/MI-NAMについて/)
-      end
-      it 'アバウト画面のリンク先が正しい' do
-          about_link = find_all('a')[2].native.inner_text
-          expect(page).to have_link about_link, href: about_path
-      end
-      it '投稿一覧のリンクが表示されている' do
-          posts_link = find_all('a')[3].native.inner_text
+      it '投稿一覧のアイコンが表示されている' do
+          posts_link = find_all('a')[5].native.inner_text
           expect(posts_link).to match(/投稿一覧/)
       end
       it '投稿一覧のリンク先が正しい' do
-          posts_link = find_all('a')[3].native.inner_text
+          posts_link = find_all('a')[5].native.inner_text
           expect(page).to have_link posts_link, href: posts_path
       end
-      it 'ログインのリンクが表示されている' do
-          login_link = find_all('a')[4].native.inner_text
-          expect(login_link).to match(/ログイン/)
+      it 'アバウト画面のリンクが表示されている' do
+          about_link = find_all('a')[6].native.inner_text
+          expect(about_link).to match(/MI-NAMについて/)
       end
-      it 'ログインのリンク先が正しい' do
-          login_link = find_all('a')[4].native.inner_text
-          expect(page).to have_link login_link, href: new_user_session_path
+      it 'アバウト画面のリンク先が正しい' do
+          about_link = find_all('a')[6].native.inner_text
+          expect(page).to have_link about_link, href: about_path
       end
       it '新規登録のリンクが表示されている' do
-          signup_link = find_all('a')[5].native.inner_text
+          signup_link = find_all('a')[7].native.inner_text
           expect(signup_link).to match(/新規登録/)
       end
       it '新規登録のリンク先が正しい' do
-          signup_link = find_all('a')[5].native.inner_text
+          signup_link = find_all('a')[7].native.inner_text
           expect(page).to have_link signup_link, href: new_user_registration_path
+      end
+      it 'ログインのリンクが表示されている' do
+          login_link = find_all('a')[8].native.inner_text
+          expect(login_link).to match(/ログイン/)
+      end
+      it 'ログインのリンク先が正しい' do
+          login_link = find_all('a')[8].native.inner_text
+          expect(page).to have_link login_link, href: new_user_session_path
       end
     end
   end
